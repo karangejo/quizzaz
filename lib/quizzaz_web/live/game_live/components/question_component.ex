3b48266399d876e485ff7fb7e7d12e9267493d47
karangejo <gabriel.g.gras@gmail.com>
2022-11-24 14:02:39 +0800

    Remove deprecated ~E sigil

--- lib/quizzaz_web/live/game_live/components/question_component.ex
+++ lib/quizzaz_web/live/game_live/components/question_component.ex
@@ -27,23 +27,29 @@ defmodule QuizzazWeb.GameLive.QuestionComponent do
   end
 
   defp add_or_update_icon(action) do
+    assigns = %{}
+
     case action do
       :new_question ->
-        ~E"""
+        ~H"""
         <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6 text-skin-secondary"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2">
-
-          <path stroke-linecap="round" stroke-linejoin="round" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 text-skin-secondary"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+          />
         </svg>
         """
 
       :edit_question ->
-        ~E"""
+        ~H"""
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6 text-skin-secondary"
